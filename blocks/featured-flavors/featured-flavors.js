f7cb92e5afb38973c995d5a2aab2c727c334ca99
Lamont Crook <lamont@adobe.com>
2022-10-03 17:30:36 -0600

    feat: featured flavors

--- blocks/featured-flavors/featured-flavors.js
+++ blocks/featured-flavors/featured-flavors.js
@@ -1,36 +1,29 @@
-const errors = {
-    header: '<h4>No Header</h4>',
-    body: '<p>No body information</p>'
+import { lookupPages, decorateIcons } from '../../scripts/scripts.js';
+
+function createFeaturedFlavorCard(page, prefix) {
+  const card = document.createElement('div');
+  const iconName = page.path.split('/').pop();
+
+  card.className = `${prefix}-card`;
+
+  card.innerHTML = `
+    
+    <h4>${page.title}</h4>
+    <p>${page.description}</p>
+    <a href=${page.path}>Learn More</a>
+    <span class="icon icon-${iconName}"></span>`;
+  return card;
 }
-import { createOptimizedPicture, lookupPages } from '../../scripts/scripts.js';
-
-async function createFeaturedFlavor(page) {
-    const row = document.createElement('div');
-    const resp = await fetch(`${page.path}.plain.html`);
-    let plain = await resp.text();
-    
-    let start = plain.search('<h1');
-    let end = plain.search('</h1>');
-    
-    let header = plain.substring(start, end>-1?end+5:0) || errors.header;
-    header = header.replaceAll('<h1', '<h4')||'No Header';
-
-    let body = errors.body;
-
-    row.innerHTML = `
-      ${header}
-          ${body}
-            <a href=${page.path}>Learn More</a>
-     
-    `;
-    return row;
-  }
 
 export default async function decorate(block) {
-    const pathnames = [...block.querySelectorAll('a')].map((a) => new URL(a.href).pathname);
-    const pages = await lookupPages(pathnames);
-    block.textContent = '';
-    pages.forEach(async (page) => {
-      block.append(await createFeaturedFlavor(page));
-    });
-  }
+  const pathnames = [...block.querySelectorAll('a')].map((a) => {
+    a.remove();
+    return new URL(a.href).pathname;
+  });
+  const pages = await lookupPages(pathnames);
+  block.innerHTML = '';
+  pages.forEach((page) => {
+    block.append(createFeaturedFlavorCard(page, 'featured-flavor'));
+  });
+  decorateIcons(block);
+}
