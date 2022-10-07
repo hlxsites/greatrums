0fed34772b90576821f46d4d03de357ad95abf24
David Nuescheler <david@nuescheler.ch>
2022-10-07 15:57:53 -0700

    chore: cocktail styling

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -4,7 +4,7 @@ export default async function decorate(block) {
   const background = document.querySelector('picture');
   if (background) background.classList.add('hero-background');
   if (block.querySelector('.icon')) block.classList.add('hero-white');
-  if (document.body.classList.contains('rum')) {
+  if (document.body.classList.contains('rum') || getMetadata('hero-overlay')) {
     const overlay = createOptimizedPicture(getMetadata('cardimage'));
     overlay.classList.add('hero-overlay');
     block.append(overlay);
