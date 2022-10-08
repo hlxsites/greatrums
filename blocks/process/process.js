6b1bd8788e65e3e13a8a7e2fe1064873a274fd20
Lamont Crook <lamont@adobe.com>
2022-10-08 11:19:26 -0600

    update process

--- blocks/process/process.js
+++ blocks/process/process.js
@@ -1,19 +1,26 @@
-import { createOptimizedPicture } from '../../scripts/scripts.js';
+import { decorateIcons } from '../../scripts/scripts.js';
+
+let i = 1;
+
+function modifyCardDescription(div) {
+  const cls = (i % 2 === 0) ? 'right' : 'left';
+  div.innerHTML = `<span class='icon icon-process-${cls}'></span><span class='card-contents'>${div.innerHTML}</span>`;
+  i += 1;
+  return ' card-description';
+}
 
 export default function decorate(block) {
-  
-    [...block.children].forEach((row) => {
-        row.className = 'process-row';
-    
+  [...block.children].forEach((row) => {
+    row.className = 'process-row';
+
     [...row.children].forEach((div) => {
-        div.className = 'process-card';
-        if(div.innerHTML == '')
-            div.innerHTML = '&nbsp;';
+      div.className = 'process-card';
 
-        
+      const cardDescription = div.getElementsByTagName('h5').length > 0 && modifyCardDescription(div);
+      const cardPicture = div.getElementsByTagName('picture').length > 0 && ' card-image';
+      div.className += cardDescription || cardPicture;
     });
-
-    console.log(row);
   });
-  
+
+  decorateIcons(block);
 }
